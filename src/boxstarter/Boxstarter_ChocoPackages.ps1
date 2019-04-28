786d578cd0f5000f361cf478c712dc92f77800de
jperdok <justin-p@users.noreply.github.com>
2019-04-28 20:33:06 +0200

    removed startups, BC still boots on startu. Only needed it for that

--- src/boxstarter/Boxstarter_ChocoPackages.ps1
+++ src/boxstarter/Boxstarter_ChocoPackages.ps1
@@ -17,55 +17,6 @@ Function RebootIfNeeded {
         Invoke-Reboot 
     }
 }
-Function Disable-Startups 
-{
-    [CmdletBinding()]
-    Param (
-        [parameter(DontShow = $true)]
-        $32bit = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run",
-        [parameter(DontShow = $true)]
-        $32bitRunOnce = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce",
-        [parameter(DontShow = $true)]
-        $64bit = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Run",
-        [parameter(DontShow = $true)]
-        $64bitRunOnce = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\RunOnce",
-        [parameter(DontShow = $true)]
-        $currentLOU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run",
-        [parameter(DontShow = $true)]
-        $currentLOURunOnce = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce",
-        [parameter()]
-        [PSCustomObject]$DisableList
-    )
-    begin {
-        New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS | 
-        out-null
-        $startups = Get-CimInstance Win32_StartupCommand | 
-        Select-Object Name,Location
-    }
-    process {
-        Get-Item -path $32bit,$32bitRunOnce,$64bit,$64bitRunOnce,$currentLOU,$currentLOURunOnce |
-        Where-Object {$_.ValueCount -ne 0} | 
-        Select-Object  @{Name = 'Location';Expression = {$_.name -replace 'HKEY_LOCAL_MACHINE','HKLM' -replace 'HKEY_CURRENT_USER','HKCU'}},
-        @{Name = 'Name';Expression = {$_.Property}} | 
-        %{
-            ForEach($disableListName in $disableList) {
-                If ($_.Name -contains $disableListName) {
-                    if ($_.Location -match 'HKLM') {
-                        Remove-ItemProperty -Path ($_.Location -Replace 'HKLM\\','HKLM:\' ) -Name $disableListName
-                    } ElseIf ($_.Location -match 'HKCU') {
-                        Remove-ItemProperty -Path ($_.Location -Replace 'HKCU\\','HKCU:\' ) -Name $disableListName
-                    }
-                }
-                Else { 
-                    Write-Warning -Message "$disableListName not found in registry" 
-                }
-            }
-        }
-    }
-    end {
-
-    }
-}
 $Boxstarter.RebootOk=$true
 $Boxstarter.NoPassword=$true
 $Boxstarter.AutoLogin=$true
@@ -127,5 +78,4 @@ $VSCodeExtensions | ForEach-Object {
 }
 Enable-UAC
 MoveShortcuts
-Disable-Startups -DisableList $DisableStartupList
 RebootIfNeeded
