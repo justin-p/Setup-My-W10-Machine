4307f439468026549b07a7e4d68a53b3b7c36272
Justin-p <justin-p@users.noreply.github.com>
2019-07-07 18:41:21 +0200

    add wsl setup to boxstarter

--- run.ps1
+++ run.ps1
@@ -21,13 +21,14 @@ InstallFonts
 InstallDotfiles
 ImportBoxstarter
 SetupGitLFS
-New-PackageFromScript src\boxstarter\Boxstarter_ChocoPackages.ps1       ChocoPackages
-New-PackageFromScript src\boxstarter\Boxstarter_PSPackages.ps1          PSPackages
-New-PackageFromScript .\src\boxstarter\Boxstarter_InstallWSLDistros.ps1 InstallWSLDistros
+New-PackageFromScript src\boxstarter\Boxstarter_ChocoPackages.ps1     ChocoPackages
+New-PackageFromScript src\boxstarter\Boxstarter_PSPackages.ps1        PSPackages
+New-PackageFromScript src\boxstarter\Boxstarter_InstallWSLDistros.ps1 InstallWSLDistros
+New-PackageFromScript src\boxstarter\Boxstarter_SetupWSLDistros.ps1   SetupWSLDistros
 If (Test-Path $(Join-Path $(Get-ScriptDirectory) 'src\Win10-Initial-Setup-Script\Win10.psm1')) {
     . $(Join-Path $(Get-ScriptDirectory) 'src\Win10-Initial-Setup-Script\Win10.ps1') -Include $(Join-Path $(Get-ScriptDirectory) 'src\Win10-Initial-Setup-Script\Win10.psm1') -Preset $(Join-Path $(Get-ScriptDirectory) 'src\Win10-Initial-Setup-Script\custom\my-home.preset')  
 }
 Else {
     Write-Output "Cant find $(Join-Path $(Get-ScriptDirectory) 'src\Win10-Initial-Setup-Script\Win10.psm1')"
 }
-Install-BoxstarterPackage -PackageName InstallWSLDistros,ChocoPackages, PSPackages
+Install-BoxstarterPackage -PackageName InstallWSLDistros, SetupWSLDistros, ChocoPackages, PSPackages
