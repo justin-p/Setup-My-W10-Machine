433f3b057cc77f9b49052cebb4fd20174c41cede
Justin Perdok <justin-p@users.noreply.github.com>
2022-01-04 18:02:25 +0100

    Update functions.ps1

--- src/functions.ps1
+++ src/functions.ps1
@@ -21,6 +21,12 @@ Function ImportBoxstarter {
         Import-Module -Name $_
     }
 }
+Function Set-BackgroundColor {
+    Param (
+        $Hex
+    )
+    Add-Type -TypeDefinition "using System;`n using System.Runtime.InteropServices;`n public class PInvoke { [DllImport(`"user32.dll`")] public static extern bool SetSysColors(int cElements, int[] lpaElements, int[] lpaRgbValues); }";[PInvoke]::SetSysColors(1, @(1), @("0x$($Hex)"))
+}
 #endregion
 #region Install Functioms
 Function InstallDependencies {
